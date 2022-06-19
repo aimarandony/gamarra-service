98c3217a31573293f64277ca37dc962b0a7d9013
Aimar Andony Berrocal Coaquira <aberrocc@emeal.nttdata.com>
2022-06-19 17:36:09 -0500

    first commit

---
+++ src/test/java/com/idat/gamarra/GamarraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.gamarra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamarraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
